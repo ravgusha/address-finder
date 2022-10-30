5dd52d86a8de851bd27815e09c1c9dfcb743f20a
ravgusha <ravgusha@gmail.com>
2022-10-31 00:25:24 +0300

    feat: add navbar scroll

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -8,7 +8,7 @@ interface ISearchBox {
 const SearchBox = ({ onSubmit }: ISearchBox) => {
   return (
     <form onSubmit={onSubmit}>
-      <input type="text" placeholder="Введите интересующий вас адрес" name="query" />
+      <input type="text" placeholder="Введите интересующий вас адрес" name="query" minLength={3} />
       <button type="submit">
         <span>Поиск</span>
       </button>
